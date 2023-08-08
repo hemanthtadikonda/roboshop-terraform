afe9cfcdaf21d9c446a61f7dd1f6cad05acb1732
hemanth <hemanthtadikonda29@gmail.com>
2023-08-08 16:49:20 +0530

    roboshop project with terraform

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ module "components" {
   for_each = var.components
 
   zone_id             = var.zone_id
-  security_groups     = var.security_groups
+  security_groups     = [var.security_groups]
   name                = each.value["name"]
   instance_type       = each.value["instance_type"]
 }
